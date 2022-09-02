8b3e5dcdd9fd1dddd78e420381fe72d72403af7c
yanad <yana.danilova1509@gmail.com>
2022-09-02 12:05:30 +0200

    Initial commit

---
+++ src/main/java/de/danilova/jspring/JspringApplication.java
@@ -0,0 +1,14 @@
+package de.danilova.jspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/danilova/jspring/JspringApplicationTests.java
@@ -0,0 +1,13 @@
+package de.danilova.jspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
