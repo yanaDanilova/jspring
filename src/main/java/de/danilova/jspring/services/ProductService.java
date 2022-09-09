c03f266ca83ec003f7772505ec3186c2fb6de340
yanad <yana.danilova1509@gmail.com>
2022-09-09 11:05:18 +0200

    homework

--- src/main/java/de/danilova/jspring/services/ProductService.java
+++ src/main/java/de/danilova/jspring/services/ProductService.java
@@ -2,7 +2,8 @@ package de.danilova.jspring.services;
 
 
 import de.danilova.jspring.models.Product;
-import de.danilova.jspring.repositories.ProductRepository;
+import de.danilova.jspring.repositories.ProductDao;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,27 +12,27 @@ import java.util.Optional;
 
 @Service
 public class ProductService {
-    private ProductRepository productRepository;
+
+    private ProductDao productDao;
 
     @Autowired
-    public ProductService(ProductRepository productRepository){
-        this.productRepository=productRepository;
+    public ProductService(ProductDao productDao){
+        this.productDao = productDao;
     }
-
     public List<Product> getAllProducts(){
-        return productRepository.getProductList();
+        return productDao.findAll();
     }
 
     public Optional<Product> getProductById(Long id){
-        return productRepository.findProductById(id);
+        return productDao.findById(id);
     }
 
 
     public void addNewProduct(Product product){
-        productRepository.addNewProduct(product);
+        productDao.saveOrUpdate(product);
     }
 
     public void deleteProductById(Long id){
-        productRepository.deleteProductById(id);
+        productDao.deleteById(id);
     }
 }
